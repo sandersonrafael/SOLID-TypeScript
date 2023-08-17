0644dcfc11c511bc6517b83cf217d3b1958429a6
Sanderson Rafael <sandersonrafael-35@hotmail.com>
2023-08-17 12:44:49 -0300

    initial config

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+const rafael = {
+    nome: 'Sanderson Rafael da Cunha Ferreira',
+    idade: 26,
+    altura: 185,
+    peso: 87,
+};
+console.log(rafael === null || rafael === void 0 ? void 0 : rafael.nome, rafael === null || rafael === void 0 ? void 0 : rafael.altura, rafael !== null && rafael !== void 0 ? rafael : 185);
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+const rafael = {
+  nome: 'Sanderson Rafael da Cunha Ferreira',
+  idade: 26,
+  altura: 185,
+  peso: 87,
+};
+
+console.log(rafael?.nome, rafael?.altura, rafael ?? 185);
